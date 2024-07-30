6bb798c4be05512967a469cf21b1b55a69116fca
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2024-07-29 20:31:50 -0400

    Start

---
+++ Project_Poland/Source/Project_Poland/Project_Poland.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_Poland.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Poland, "Project_Poland" );
